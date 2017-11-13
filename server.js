db3dcd464e1555624dcb9758a39ed1a299a1e71e
leyloo <Sondley1@gmail.com>
2017-11-13 19:55:43 -0400

    second commit

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express'),
+app = express(),
+port = process.env.PORT || 3000,
+mongoose = require('mongoose'),
+Task = require('./api/models/todoListModel'), //created model loading here
+bodyParser = require('body-parser');
+var connectMongoOnline='mongodb://leyloo:Sondley2318@ds259245.mlab.com:59245/sondley';
+// mongoose instance connection url connection
+mongoose.Promise = global.Promise;
+mongoose.connect(connectMongoOnline); 
+//mongoose.connect('mongodb://localhost/Tododb'); 
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+var routes = require('./api/routes/todoListRoutes'); //importing route
+routes(app); //register the route
+
+
+app.listen(port);
+
+
+console.log('todo list RESTful API server started on: ' + port);
+
